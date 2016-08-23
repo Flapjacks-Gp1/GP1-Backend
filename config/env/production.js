a99730374563b6e7c760610bbac1154580665c78
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-23 16:46:33 +0800

    changed mongodb uri

--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,5 @@
 //   db: 'mongodb://anu123:anu123@ds161295.mlab.com:61295/anu-mymdb', // diff url for heroku
 // };
 module.exports = {
-  db: process.env.PROD_MONGODB, // diff url for heroku
+  db: process.env.MONGODB_URI // diff url for heroku
 };
