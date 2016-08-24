01d96dc21eeafa7c2ff1dcd617e6c40f3bf76225
kit jonathan ho <j.hokit80@gmail.com>
2016-08-24 17:18:55 +0800

    configured blacklist to enabled logout

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 var config = require('./config'),
   mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 module.exports = function() {
   var db = mongoose.connect(config.db);
   require('../models/event');
